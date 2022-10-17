27514c1f5fdb4973182407b2073d8d9f9510fc4d
Paul Gavrikov <paul.gavrikov@hs-offenburg.de>
2022-10-17 21:38:14 +0000

    adding kaggle-style imagenet

--- timm/data/dataset_factory.py
+++ timm/data/dataset_factory.py
@@ -30,6 +30,47 @@ _TRAIN_SYNONYM = dict(train=None, training=None)
 _EVAL_SYNONYM = dict(val=None, valid=None, validation=None, eval=None, evaluation=None)
 
 
+from torch.utils.data import Dataset
+from PIL import Image
+import json
+
+
+class ImageNetKaggle(Dataset):
+    def __init__(self, root, split, transform=None):
+        self.samples = []
+        self.targets = []
+        self.transform = transform
+        self.syn_to_class = {}
+        with open(os.path.join(root, "imagenet_class_index.json"), "rb") as f:
+                    json_file = json.load(f)
+                    for class_id, v in json_file.items():
+                        self.syn_to_class[v[0]] = int(class_id)
+        with open(os.path.join(root, "ILSVRC2012_val_labels.json"), "rb") as f:
+                    self.val_to_syn = json.load(f)
+        samples_dir = os.path.join(root, "ILSVRC/Data/CLS-LOC", split)
+        for entry in os.listdir(samples_dir):
+            if split == "train":
+                syn_id = entry
+                target = self.syn_to_class[syn_id]
+                syn_folder = os.path.join(samples_dir, syn_id)
+                for sample in os.listdir(syn_folder):
+                    sample_path = os.path.join(syn_folder, sample)
+                    self.samples.append(sample_path)
+                    self.targets.append(target)
+            elif split == "val":
+                syn_id = self.val_to_syn[entry]
+                target = self.syn_to_class[syn_id]
+                sample_path = os.path.join(samples_dir, entry)
+                self.samples.append(sample_path)
+                self.targets.append(target)
+    def __len__(self):
+                return len(self.samples)
+    def __getitem__(self, idx):
+                x = Image.open(self.samples[idx]).convert("RGB")
+                if self.transform:
+                    x = self.transform(x)
+                return x, self.targets[idx]
+
 def _search_split(root, split):
     # look for sub-folder with name of split in root and use that if it exists
     split_name = split.split('[')[0]
@@ -125,7 +166,7 @@ def create_dataset(
         elif name == 'imagenet':
             if split in _EVAL_SYNONYM:
                 split = 'val'
-            ds = ImageNet(split=split, **torch_kwargs)
+            ds = ImageNetKaggle(root="/workspace/data/datasets/imagenet1k", split=split)
         elif name == 'image_folder' or name == 'folder':
             # in case torchvision ImageFolder is preferred over timm ImageDataset for some reason
             if search_split and os.path.isdir(root):
